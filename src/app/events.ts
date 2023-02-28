01b5fe147b63c24559aba50c1bc7bf27566a102f
Thomas Netusil <thomas.netusil@gmail.com>
2023-02-28 13:53:49 +0100

    changes

--- src/app/events.ts
+++ src/app/events.ts
@@ -20,10 +20,10 @@ export class event {
 }
 
 export let events = [
-  new event("Change your career!", "05.03.2023&06.03.2023", "Meet us at Bestmesse Wieaner Stadthalle Vienna", "mese.png", 0, true, 0),
-  new event("Basic HTML/CSS course", "11.04.2023&12.04.2023", "Small changes can change your life", "htmlkurs.png", 299, true, 0),
+  new event("Change your career!", "05.03.2023 & 06.03.2023", "Meet us at Bestmesse Wiener Stadthalle Vienna", "mese.png", 0, true, 0),
+  new event("Basic HTML/CSS course", "11.04.2023 & 12.04.2023", "Small changes can change your life", "htmlkurs.png", 299, true, 0),
   new event("Basic Python Programming course", "17.04.2023", "Change your Job!", "python.png", 590, true, 0),
-  new event("Backend Developer", "13.04.2023&27.04.2023", "Start new career with us", "backend.png", 2990, true, 0),
-  new event("Party at Code Factory", "27.04.2023 Start: 17:00", "Meet and Drink", "wallstreet-580.webp", 0, true, 0),
-  new event("Frontend Developer", "15.05.2023&06.07.2023", "Learn coding in 2 months", "frontend.png", 2990, true, 0)
+  new event("Backend Developer", "13.04.2023 & 27.04.2023", "Start new career with us", "backend.png", 2990, true, 0),
+  new event("Party at Code Factory", "27.04.2023 Start: 5pm", "Meet and Drink", "wallstreet-580.webp", 0, true, 0),
+  new event("Frontend Developer", "15.05.2023 & 06.07.2023", "Learn coding in 2 months", "frontend.png", 2990, true, 0)
 ];
