5ed01414f04ca9b04a58a38a6914b17ff82aa805
Thomas Netusil <thomas.netusil@gmail.com>
2023-03-01 17:28:04 +0100

    update studentdetails

--- src/app/studentsdetails/studentsdetails.component.ts
+++ src/app/studentsdetails/studentsdetails.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { IStudent } from '../IStudentsdetails';
+import { student, students } from '../studentsdetails';
 @Component({
   selector: 'app-studentsdetails',
   templateUrl: './studentsdetails.component.html',
   styleUrls: ['./studentsdetails.component.scss']
 })
 export class StudentsdetailsComponent {
-
+  students: Array<IStudent> = students;
+  student: IStudent = {} as IStudent;
 }
- 
