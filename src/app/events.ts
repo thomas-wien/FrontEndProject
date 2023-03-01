d025d07bb9cf92081a40b40fd777fabcf2554898
Aleks1411 <addastefanovski@gmail.com>
2023-03-01 09:17:19 +0100

    update

--- src/app/events.ts
+++ src/app/events.ts
@@ -20,7 +20,7 @@ export class event {
 }
 
 export let events = [
-  new event("Change your career!", "05.03.2023 & 06.03.2023", "Meet us at Bestmesse Wiener Stadthalle Vienna", "mese.png", 0, true, 0),
+  new event("Change your career!", "02.03.2023 & 05.03.2023", "Meet us at Bestmesse Wiener Stadthalle Vienna", "mese.png", 0, true, 0),
   new event("Basic HTML/CSS course", "11.04.2023 & 12.04.2023", "Small changes can change your life", "htmlkurs.png", 299, true, 0),
   new event("Basic Python Programming course", "17.04.2023", "Change your Job!", "python.png", 590, true, 0),
   new event("Backend Developer", "13.04.2023 & 27.04.2023", "Start new career with us", "backend.png", 2990, true, 0),
